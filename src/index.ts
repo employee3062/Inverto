d9f7db0598733689c23366e3b86e99f4bed68a0b
ToxicalNoob3062 <rahat3062@gmail.com>
2025-09-13 13:43:22 -0400

    organized elysia js structure and it's health check

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,17 @@
 import { Elysia } from "elysia";
+import { swagger } from '@elysiajs/swagger'
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+export const app = new Elysia().get("/", () => "It's Inverto").listen(3000);
+app.use(
+  swagger({
+    documentation: {
+        info: {
+          title: 'Inverto API',
+          version: '1.0.0'
+        }
+      }
+  })
+);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
