89a90544b865eb3de03bdb8ea5de0f7feda8f7e7
ToxicalNoob3062 <rahat3062@gmail.com>
2025-09-18 11:32:20 -0400

    feat: Enhance auto-deploy workflow and update test cases for versioning

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { Elysia , t} from "elysia";
 import { swagger } from '@elysiajs/swagger'
 
 const app = new Elysia()
-.get("/", () => "It's Inverto 9")
+.get("/", () => "It's Inverto v9")
 .post("/signin",({body}) => {
   return `Created user ${body.username} with password ${body.password}.`
 } ,{
